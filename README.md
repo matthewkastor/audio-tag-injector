7712c04abfde2b4894d38f80f4615296be765a5c
Matthew Kastor <matthewkastor@gmail.com>
2013-07-26 15:19:19 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+#audio-tag-injector
+
+Automatically adds html5 audio elements inline beside links to mp3, ogg, and 
+wav files.
+
+## Install it on node from npm
+
+`npm install audio-tag-injector`
+
+## Usage
+
+If you're using this module outside of a browser you will have to supply a 
+reference to a dom document object, unless you've called it `document`. I did 
+not require any specific module for parsing the DOM, because there are a few 
+out there and it would be rude of me to force you to use a specific one for 
+such a simple function.
+
+### Injection of Audio Tags in Browser
+
+```
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <script src="./src/audio-tag-injector.js"
+              type="text/javascript">
+</script>
+        <title>
+            Example Usage of audio-tag-injector.js
+        </title>
+    </head>
+    <body>
+        <h1>
+            Example Usage of audio-tag-injector.js
+        </h1>
+
+        <p>
+            Ooh! I made some audio! Check it out <a href="/some_audio_file.mp3">Kastor at the mic!</a>
+        </p>
+        <script type="text/javascript">
+
+            audioTagInjector();
+        </script>
+    </body>
+</html>
+```
+
+### Injection of Audio Tags in Node
+
+```
+var audioTagInjector = require('audio-tag-injector');
+// parse your html into a DOM Document
+
+// the argument is optional and defaults to `document`, if you've named your
+// DOM Document something else you'll have to supply a refrence to it. For 
+// this example, let's say you called it `myDocument`.
+audioTagInjector(myDocument);
+
+// Shazam. The document now contains audio links you didn't have to write.
+```
---
+++ src/audio-tag-injector.js
@@ -0,0 +1,84 @@
+/*jslint
+    white: true,
+    browser: true,
+    vars: true
+*/
+
+/**
+ * Automatically adds html5 audio elements inline beside links to
+ *  mp3, ogg, and wav files. Includes an event handler to show an
+ *  error message if the audio file fails to load. Includes an error message
+ *  if the audio element is not supported.
+ * @param {HTMLDOMDocument} documentRef Optional A reference to the document
+ *  object. Defaults to `document`.
+ * @author Matthew Christopher Kastor-Inare III
+ * @version 20130726
+ * @example
+ *  audioTagInjector();
+ */
+function audioTagInjector (documentRef) {
+    "use strict";
+    documentRef = documentRef || document;
+    function getMimeType (href) {
+        var type = null;
+        switch (true) {
+            case /\.mp3$/.test(href) :
+                type = 'audio/mpeg';
+                break;
+            case /\.ogg$/.test(href) :
+                type = 'audio/ogg';
+                break;
+            case /\.wav$/.test(href) :
+                type = 'audio/wav';
+                break;
+            default :
+                break;
+        }
+        return type;
+    }
+    function generateAudioTag (linkElement) {
+        function errorHandler (ev) {
+            var d = documentRef.createElement('span');
+            var audio = ev.target.parentNode;
+            d.textContent = 'Audio file failed to load!';
+            audio.parentNode.replaceChild(d, audio);
+        }
+        var audio = documentRef.createElement('audio');
+        var source = documentRef.createElement('source');
+        var sourceType = getMimeType(linkElement.href);
+        var unavailable = documentRef.createElement('p');
+        
+        audio.setAttribute('controls', 'controls');
+        audio.setAttribute('preload', 'none');
+        
+        source.setAttribute('src', linkElement.href);
+        source.setAttribute('type', sourceType);
+        // show error message if audio fails to load.
+        source.addEventListener('error', errorHandler, false);
+        
+        unavailable.textContent = 'Your browser does not support the audio element.';
+        
+        audio.appendChild(source);
+        audio.appendChild(unavailable);
+        
+        // break link to new line
+        linkElement.parentNode.insertBefore(documentRef.createElement('br'), linkElement);
+        // insert audio before the link
+        linkElement.parentNode.insertBefore(audio, linkElement);
+        // break between inserted audio and link
+        linkElement.parentNode.insertBefore(documentRef.createElement('br'), linkElement);
+        // break after link
+        linkElement.parentNode.insertBefore(documentRef.createElement('br'), linkElement.nextSibling);
+    }
+    Array.prototype.slice.call(documentRef.links).forEach(function (link) {
+        if(null !== getMimeType(link.href)) {
+            generateAudioTag(link);
+        }
+    });
+}
+
+try {
+     module.exports = audioTagInjector;
+} catch (e) {
+    // module.exports is not defined
+}
